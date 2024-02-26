cbe1e78fcc92db85526761d11b534c3274e7cdfe
ReeFDQ <156304001+ReeFDQ@users.noreply.github.com>
2024-02-26 15:20:48 +0700

    Add files via upload

---
+++ Dental Clinic/main.js
@@ -0,0 +1,11 @@
+// Swiper 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
